1cf21d866f0890b44ba939b5d18daa614174975b
Andres-01 <andressantiagoortiz@institutolosandes.edu.ec>
2022-11-10 18:02:32 -0500

    Archivos de inscripcion

---
+++ src/links.js
@@ -0,0 +1,43 @@
+export const mainContent = {
+  name: `INSTITUTO SUPERIOR TECNOLÓGICO "LOS ANDES"`,
+  imgSrc: `/assets/log.png`,
+};
+
+export const socialLinks = [
+  {
+    title: 'Web',
+    icon: 'mdi:web',
+    url: 'https://institutolosandes.edu.ec/',
+  },
+  {
+    title: 'Facebook',
+    icon: 'mdi:facebook  ',
+    url: 'https://www.facebook.com/institutolosandesloja',
+  },
+  {
+    title: 'Instagram',
+    icon: 'mdi:instagram',
+    url: 'https://www.instagram.com/tecnologico_los_andes/',
+  },
+  {
+    title: 'WhatsApp',
+    icon: 'mdi:whatsapp',
+    url: 'https://wa.me/593984344331?text=Hola%20quiero%20mas%20informacion%20sobre%20las%20carreras',
+  },
+]
+
+export const links = [
+  {
+    text: 'Sistema de Becas',
+    url: 'https://littlesticks.dev',
+    featured: true,
+  },
+  {
+    text: 'Sistema de Matriculas',
+    url: 'https://jaydanurwin.com/about',
+  },
+  {
+    text: 'Registro',
+    url: 'https://jaydanurwin.com/uses',
+  },
+]
